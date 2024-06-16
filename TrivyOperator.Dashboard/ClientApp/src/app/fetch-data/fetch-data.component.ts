d5db7af1824bbbd490d4cf97d71ca8969bc1c871
raoul.precupas <raoul.precupas@druidai.com>
2024-06-16 03:07:06 +0300

    grid tryouts

--- TrivyOperator.Dashboard/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ TrivyOperator.Dashboard/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,20 +1,59 @@
-import {Component} from '@angular/core';
-import {ColDef} from "ag-grid-community";
+import { Component } from '@angular/core';
+import {
+  ColDef,
+  ColGroupDef,
+  DataTypeDefinition,
+  GridApi,
+  GridOptions,
+  GridReadyEvent,
+  createGrid,
+  ValueGetterParams,
+  CsvExportParams,
+} from "ag-grid-community";
 import { VulnerabilityReportsService } from "../../api/services/vulnerability-reports.service";
 import { VulnerabilityDto } from "../../api/models/vulnerability-dto";
+import { VulnerabilitSeverityRenderer } from "./vulnerabilit-severity-renderer.component";
 
 @Component({
   selector: 'app-fetch-data',
   templateUrl: './fetch-data.component.html',
 })
+
 export class FetchDataComponent {
+  public rowSelection: "single" | "multiple" = "multiple";
+  private gridExportParams: CsvExportParams = { columnSeparator: ",", onlySelected: true };
+  private gridApi!: GridApi;
+
   public vulnerabilities: VulnerabilityDto[] = [];
+
   public columnDefs: ColDef[] = [
-    {headerName: 'Date', field: "date", filter: true, flex: 1},
-    {headerName: 'Temp. (C)', field: "temperatureC", filter: true, flex: 1},
-    {headerName: 'Temp. (F)', field: "temperatureF", filter: true, flex: 1},
-    {headerName: 'Summary', field: "summary", filter: true, flex: 1}
+    { headerName: 'Namespace', field: "namespace", filter: true, flex: 3 },
+    { headerName: 'Container', field: "containerName", filter: true, flex: 3 },
+    {
+      headerName: 'Image Name and Tag', field: "imageName", filter: true, flex: 9,
+      valueGetter: (params: ValueGetterParams) =>
+        params.data.imageName + ":" + params.data.imageTag,
+    },
+    { headerName: 'S', field: "severity", filter: true, flex: 1, minWidth: 60, cellRenderer: VulnerabilitSeverityRenderer },
+    { headerName: 'Title', field: "title", filter: true, flex: 15, wrapText: true, autoHeight: true },
+    {
+      headerName: 'CVE', field: "vulnerabilityId", filter: true, flex: 6,
+      cellRenderer: (params: ValueGetterParams) => (`<a href="${params.data.primaryLink}" target="_blank">${params.data.vulnerabilityId}</a>`)
+    },
   ];
+
+  public defaultColDef: ColDef = {
+    floatingFilter: true,
+    suppressFloatingFilterButton: true,
+  };
+
+  onBtnExport() {
+    this.gridApi.exportDataAsCsv(this.gridExportParams);
+  }
+
+  onGridReady(params: GridReadyEvent) {
+    this.gridApi = params.api;
+  }
 
   constructor(vulnerabilityReportsService: VulnerabilityReportsService) {
     vulnerabilityReportsService.getAll$Json().subscribe(result => this.vulnerabilities = result, error => console.error(error))
