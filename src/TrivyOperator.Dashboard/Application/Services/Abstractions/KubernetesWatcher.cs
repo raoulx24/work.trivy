c47f8eb92eb8482e34e0623019154cfad5d690f4
raoul.precupas <raoul.precupas@druidai.com>
2024-06-29 01:37:02 +0300

    first watchers

---
+++ src/TrivyOperator.Dashboard/Application/Services/Abstractions/KubernetesWatcher.cs
@@ -0,0 +1,52 @@
+﻿using k8s;
+using k8s.Autorest;
+using k8s.Models;
+using System.Net;
+using TrivyOperator.Dashboard.Domain.Trivy.CustomResources.Abstractions;
+using TrivyOperator.Dashboard.Domain.Trivy.VulnerabilityReport;
+
+namespace TrivyOperator.Dashboard.Application.Services.Abstractions;
+
+public abstract class KubernetesWatcher<TK8sObjectList, TKubernetesObject> : IKubernetesWatcher<TK8sObjectList, TKubernetesObject>
+    where TKubernetesObject : IKubernetesObject
+    where TK8sObjectList : IKubernetesObject, IItems<TKubernetesObject>
+
+
+{
+    protected ILogger logger;
+
+    public async Task Watch(Kubernetes k8sClient, CancellationToken cancellationToken, string? k8sNamespace = null)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            try
+            {
+                Task<HttpOperationResponse<TK8sObjectList>> listNamespaceResp = GetKubernetesObjectWatchList(k8sClient, k8sNamespace, cancellationToken);
+                await foreach ((WatchEventType type, TKubernetesObject item) in listNamespaceResp
+                                   .WatchAsync<TKubernetesObject, TK8sObjectList>(
+                                       ex => logger.LogError(
+                                           $"{nameof(KubernetesWatcher<TK8sObjectList, TKubernetesObject>)} - WatchAsync - {ex.Message}",
+                                           ex),
+                                       cancellationToken))
+                {
+                    await ProcessWatchEvent(type, item, k8sNamespace, cancellationToken);
+                }
+            }
+            catch (HttpOperationException hoe) when (hoe.Response.StatusCode == HttpStatusCode.NotFound)
+            {
+                // TODO: something something
+            }
+            catch (HttpOperationException hoe) when (hoe.Response.StatusCode == HttpStatusCode.Forbidden)
+            {
+                // TODO: something something
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"{nameof(KubernetesWatcher<TK8sObjectList, TKubernetesObject>)} - {ex.Message}", ex);
+            }
+        }
+    }
+
+    protected abstract Task<HttpOperationResponse<TK8sObjectList>> GetKubernetesObjectWatchList(Kubernetes k8sClient, string? k8sNamespace, CancellationToken cancellationToken);
+    protected abstract Task ProcessWatchEvent(WatchEventType type, TKubernetesObject item, string? k8sNamespace, CancellationToken cancellationToken);
+}
---
+++ src/TrivyOperator.Dashboard/Application/Services/Watchers/NamespaceWatcher.cs
@@ -0,0 +1,27 @@
+﻿using k8s;
+using k8s.Autorest;
+using k8s.Models;
+using TrivyOperator.Dashboard.Application.Services.Abstractions;
+
+namespace TrivyOperator.Dashboard.Application.Services.Watchers;
+
+public class NamespaceWatcher : KubernetesWatcher<V1NamespaceList, V1Namespace>
+{
+    public NamespaceWatcher(ILogger<NamespaceWatcher> logger)
+    {
+        this.logger = logger;
+    }
+
+    protected override async Task<HttpOperationResponse<V1NamespaceList>> GetKubernetesObjectWatchList(Kubernetes k8sClient, string? k8sNamespace, CancellationToken cancellationToken)
+    {
+        return await k8sClient.CoreV1.ListNamespaceWithHttpMessagesAsync(
+                        watch: true,
+                        timeoutSeconds: int.MaxValue,
+                        cancellationToken: cancellationToken);
+    }
+
+    protected override Task ProcessWatchEvent(WatchEventType type, V1Namespace item, string? k8sNamespace, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
