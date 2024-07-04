81c5ed45c24e330fc9e917a582101b596e4fe41e
raoul.precupas <raoul.precupas@druidai.com>
2024-07-05 01:01:27 +0300

    new KubernetesClusterScopedCacheRefresh (not finished)

---
+++ src/TrivyOperator.Dashboard/Application/Services/CacherRefresh/Abstractions/KubernetesClusterScopedCacheRefresh.cs
@@ -0,0 +1,29 @@
+﻿using k8s;
+using TrivyOperator.Dashboard.Application.Services.BackgroundQueues.Abstractions;
+using TrivyOperator.Dashboard.Application.Services.WatcherEvents.Abstractions;
+using TrivyOperator.Dashboard.Infrastructure.Abstractions;
+
+namespace TrivyOperator.Dashboard.Application.Services.CacherRefresh.Abstractions;
+
+public abstract class KubernetesClusterScopedCacheRefresh<TKubernetesObject, TKubernetesWatcherEvent, TBackgroundQueue> :
+    CacheRefresh<TKubernetesObject, TKubernetesWatcherEvent, TBackgroundQueue>
+        where TKubernetesWatcherEvent : IKubernetesWatcherEvent<TKubernetesObject>
+        where TKubernetesObject : IKubernetesObject
+        where TBackgroundQueue : IBackgroundQueue<TKubernetesWatcherEvent, TKubernetesObject>
+{
+    protected KubernetesClusterScopedCacheRefresh(TBackgroundQueue backgroundQueue, 
+        IConcurrentCache<string, List<TKubernetesObject>> cache, 
+        ILogger<CacheRefresh<TKubernetesObject, TKubernetesWatcherEvent, TBackgroundQueue>> logger) 
+        : base(backgroundQueue, cache, logger)
+    { }
+
+    protected override void ProcessAddEvent(TKubernetesWatcherEvent watcherEvent)
+    {
+        
+    }
+
+    protected override void ProcessDeleteEvent(TKubernetesWatcherEvent watcherEvent)
+    {
+
+    }
+}
