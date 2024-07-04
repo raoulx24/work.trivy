33a5ab805cf10dbebb878fd12ee24f393df10171
raoul.precupas <raoul.precupas@druidai.com>
2024-07-04 23:23:05 +0300

    CacheRefresh

---
+++ src/TrivyOperator.Dashboard/Application/Services/CacherRefresh/Abstractions/CacheRefresh.cs
@@ -0,0 +1,47 @@
+﻿using k8s;
+using System.Linq.Expressions;
+using TrivyOperator.Dashboard.Application.Services.BackgroundQueues.Abstractions;
+using TrivyOperator.Dashboard.Application.Services.WatcherEvents.Abstractions;
+using TrivyOperator.Dashboard.Infrastructure.Abstractions;
+using TrivyOperator.Dashboard.Infrastructure.Services;
+
+namespace TrivyOperator.Dashboard.Application.Services.CacherRefresh.Abstractions;
+
+public abstract class CacheRefresh<TKubernetesObject, TKubernetesWatcherEvent, TBackgroundQueue>
+        where TKubernetesWatcherEvent : IKubernetesWatcherEvent<TKubernetesObject>
+        where TKubernetesObject : IKubernetesObject
+        where TBackgroundQueue : IBackgroundQueue<TKubernetesWatcherEvent, TKubernetesObject>
+{
+    protected TBackgroundQueue backgroundQueue { get; init; }
+    protected IConcurrentCache<string, List<TKubernetesObject>> cache { get; init; }
+    protected ILogger<CacheRefresh<TKubernetesObject, TKubernetesWatcherEvent, TBackgroundQueue>> logger { get; init; }
+
+    public CacheRefresh(TBackgroundQueue backgroundQueue,
+        IConcurrentCache<string, List<TKubernetesObject>> cache,
+        ILogger<CacheRefresh<TKubernetesObject, TKubernetesWatcherEvent, TBackgroundQueue>> logger)
+    {
+        this.backgroundQueue = backgroundQueue;
+        this.cache = cache;
+        this.logger = logger;
+    }
+
+    public async Task ProcessChannelMessage(CancellationToken cancellationToken)
+    {
+        TKubernetesWatcherEvent watcherEvent = await backgroundQueue.DequeueAsync(cancellationToken);
+        switch (watcherEvent.WatcherEvent)
+        {
+            case WatchEventType.Added:
+                ProcessAddEvent(watcherEvent);
+                break;
+            case WatchEventType.Deleted:
+                ProcessDeleteEvent(watcherEvent);
+                break;
+            //default:
+            //    break;
+        }
+
+    }
+
+    protected abstract void ProcessAddEvent(TKubernetesWatcherEvent watcherEvent);
+    protected abstract void ProcessDeleteEvent(TKubernetesWatcherEvent watcherEvent);
+}
