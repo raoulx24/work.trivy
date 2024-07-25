c216e730cbcee7387e8766e8f6b8bc568d0c4218
raoul.precupas <raoul.precupas@druidai.com>
2024-07-25 23:09:39 +0300

    new empty component

---
+++ src/TrivyOperator.Dashboard/ClientApp/src/app/alerts/alerts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alerts',
+  templateUrl: './alerts.component.html',
+  styleUrl: './alerts.component.scss'
+})
+export class AlertsComponent {
+
+}
--- src/TrivyOperator.Dashboard/ClientApp/src/app/app.module.ts
+++ src/TrivyOperator.Dashboard/ClientApp/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { VulnerabilityReportsComponent } from './vulnerability-reports/vulnerability-reports.component';
 import { VulnerabilityReportsDetailedComponent } from './vulnerability-reports-detailed/vulnerability-reports-detailed.component';
+import { AlertsComponent } from './alerts/alerts.component';
 import { AgGridModule } from "ag-grid-angular";
 import { ApiModule } from "../api/api.module";
 import { environment } from "../environments/environment";
@@ -23,6 +24,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HomeComponent,
     VulnerabilityReportsComponent,
     VulnerabilityReportsDetailedComponent,
+    AlertsComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
@@ -32,6 +34,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'vulnerability-reports', component: VulnerabilityReportsComponent },
       { path: 'vulnerability-reports-detailed', component: VulnerabilityReportsDetailedComponent },
+      { path: 'alerts', component: AlertsComponent ,}
     ]),
     AgGridModule,
     ApiModule.forRoot({rootUrl: environment.baseUrl}),
