51281ffbc738b3f5d5da82adff7b550c4652fe70
raoulx24 <raoulx24@yahoo.com>
2024-10-22 23:30:41 +0300

    home - panel states (expanded/collapsed) are saved/restored

--- src/TrivyOperator.Dashboard/ClientApp/src/app/home/home.component.ts
+++ src/TrivyOperator.Dashboard/ClientApp/src/app/home/home.component.ts
@@ -14,4 +14,37 @@ import { HomeVulnerabilityReportsComponent } from '../home-vulnerability-reports
 })
 
 export class HomeComponent {
+  public expandedPanels: string[] = ["vr"];
+
+  private readonly localstorageKeyPrefix = "Home.";
+  public isVrPanelCollapsed: boolean = false;
+
+  constructor() {
+    this.expandPanels();
+  }
+
+  onVrPanelChange(event: boolean) {
+    this.onPanelChange("vr", event);
+  }
+
+  onLiPanelChange(event: boolean) {
+    this.onPanelChange("li", event);
+  }
+
+  private onPanelChange(panelKey: string, isCollapsed: boolean) {
+    if (isCollapsed) {
+      this.expandedPanels = this.expandedPanels.filter(x => x !== panelKey);
+    }
+    else {
+      this.expandedPanels.push(panelKey);
+    }
+    localStorage.setItem(this.localstorageKeyPrefix + "PanelStates", this.expandedPanels.join(","));
+  }
+
+  private expandPanels() {
+    let savedStates = localStorage.getItem(this.localstorageKeyPrefix + "PanelStates");
+    this.expandedPanels = savedStates !== null
+      ? savedStates.split(",")
+      : this.expandedPanels;
+  }
 }
