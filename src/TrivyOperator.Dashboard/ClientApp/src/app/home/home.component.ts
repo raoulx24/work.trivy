a03d7a5a9c366fc26dc5f394c5e53fd81b1f81e2
raoulx24 <raoulx24@yahoo.com>
2024-10-23 19:38:37 +0300

    rehaul of home page

--- src/TrivyOperator.Dashboard/ClientApp/src/app/home/home.component.ts
+++ src/TrivyOperator.Dashboard/ClientApp/src/app/home/home.component.ts
@@ -1,50 +1,22 @@
 import { Component } from '@angular/core';
-
-import { PanelModule } from 'primeng/panel';
+import { CommonModule } from '@angular/common';
 
 import { HomeVulnerabilityReportsComponent } from '../home-vulnerability-reports/home-vulnerability-reports.component'
+
+import { PanelModule } from 'primeng/panel';
+import { CarouselModule } from 'primeng/carousel';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HomeVulnerabilityReportsComponent, PanelModule],
+  imports: [CommonModule, HomeVulnerabilityReportsComponent, CarouselModule, PanelModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 
 export class HomeComponent {
-  public expandedPanels: string[] = ["vr"];
-
-  private readonly localstorageKeyPrefix = "Home.";
-  public isVrPanelCollapsed: boolean = false;
-
+  public slides: string[] = ["vr", "lorem"];
   constructor() {
-    this.expandPanels();
-  }
-
-  onVrPanelChange(event: boolean) {
-    this.onPanelChange("vr", event);
-  }
-
-  onLiPanelChange(event: boolean) {
-    this.onPanelChange("li", event);
-  }
-
-  private onPanelChange(panelKey: string, isCollapsed: boolean) {
-    if (isCollapsed) {
-      this.expandedPanels = this.expandedPanels.filter(x => x !== panelKey);
-    }
-    else {
-      this.expandedPanels.push(panelKey);
-    }
-    localStorage.setItem(this.localstorageKeyPrefix + "PanelStates", this.expandedPanels.join(","));
-  }
-
-  private expandPanels() {
-    let savedStates = localStorage.getItem(this.localstorageKeyPrefix + "PanelStates");
-    this.expandedPanels = savedStates !== null
-      ? savedStates.split(",")
-      : this.expandedPanels;
   }
 }
