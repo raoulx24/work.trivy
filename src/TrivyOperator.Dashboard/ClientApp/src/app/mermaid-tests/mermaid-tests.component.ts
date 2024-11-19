3c7f8283179789513969dfa02dd4847a5f30f0de
raoulx24 <raoulx24@yahoo.com>
2024-11-19 20:08:45 +0200

    mermaid wip

--- src/TrivyOperator.Dashboard/ClientApp/src/app/mermaid-tests/mermaid-tests.component.ts
+++ src/TrivyOperator.Dashboard/ClientApp/src/app/mermaid-tests/mermaid-tests.component.ts
@@ -1,5 +1,6 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-declare var mermaid: any;
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+declare const mermaid: any;
+import * as svgPanZoom from 'svg-pan-zoom'
 
 @Component({
   selector: 'app-mermaid-tests',
@@ -9,6 +10,23 @@ declare var mermaid: any;
   styleUrl: './mermaid-tests.component.scss'
 })
 export class MermaidTestsComponent implements OnInit, AfterViewInit {
+  @ViewChild("mermaid") mermaid!: ElementRef;
+  panZoom: any;
+
+  config = {
+    theme: "neutral",
+    startOnLoad: false,
+    securityLevel: "loose",
+    flowChart: {
+      useMaxWidth: true,
+      htmlLabels: true,
+    },
+    themeVariables: {
+      fontSize: "12px",
+      primaryColor: "#607D8B",
+    },
+  };
+
   constructor() {
   }
 
@@ -16,10 +34,43 @@ export class MermaidTestsComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    const mermaidConfig = {
-      startOnLoad: true
-    };
-    mermaid.initialize(mermaidConfig);
-    mermaid.contentLoaded();
+    mermaid.initialize({
+      ...this.config,
+    });
+    setTimeout(() => {
+      mermaid.init();
+      const element = this.mermaid.nativeElement;
+      const svg = element.querySelector('svg');
+      this.panZoom = svgPanZoom(svg, {
+        panEnabled: true,
+        zoomEnabled: true,
+        mouseWheelZoomEnabled: true,
+        preventMouseEventsDefault: true,
+        center: false
+      });
+
+      console.log(JSON.stringify(this.panZoom));
+      console.log(svg);
+
+    }, 200);
   }
+
+
+  onZoomInClick() {
+    console.log(JSON.stringify(this.panZoom));
+    console.log(this.panZoom);
+    this.panZoom.zoomIn()
+  }
+
+  onZoomOutClick() {
+    console.log(JSON.stringify(this.panZoom));
+    console.log(this.panZoom);
+    this.panZoom.zoomOut()
+  }
+
+    //const mermaidConfig = {
+    //  startOnLoad: true
+    //};
+    //mermaid.initialize(mermaidConfig);
+    //mermaid.contentLoaded();
 }
