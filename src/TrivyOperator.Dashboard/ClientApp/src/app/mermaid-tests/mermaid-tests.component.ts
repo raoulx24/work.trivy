6f977d6c4c4b166fe2de00e016691bac798634bd
raoulx24 <raoulx24@yahoo.com>
2024-11-22 01:41:28 +0200

    dinamic mermeaid graph

--- src/TrivyOperator.Dashboard/ClientApp/src/app/mermaid-tests/mermaid-tests.component.ts
+++ src/TrivyOperator.Dashboard/ClientApp/src/app/mermaid-tests/mermaid-tests.component.ts
@@ -1,11 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 declare const mermaid: any;
 import * as svgPanZoom from 'svg-pan-zoom'
+
+export interface MermaidLink {
+  sourceId: string,
+  destId: string,
+  counter: number,
+}
+
+export interface MermaidNode {
+  id: string,
+  line1: string | undefined,
+  line2: string | undefined,
+  counter: number | undefined,
+}
 
 @Component({
   selector: 'app-mermaid-tests',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './mermaid-tests.component.html',
   styleUrl: './mermaid-tests.component.scss'
 })
@@ -28,7 +43,32 @@ export class MermaidTestsComponent implements OnInit, AfterViewInit {
     },
   };
 
-  constructor() {
+  nodes: MermaidNode[] = [];
+  links: MermaidLink[] = [];
+
+  mermaidGraphDefinition: SafeHtml;
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.nodes.push({ id: "A", line1: "A - Lorem ipsum dolor", line2: "sit amet", counter: 0 });
+    this.nodes.push({ id: "B", line1: "B - Lorem ipsum dolor", line2: "sit amet", counter: 0 });
+    this.nodes.push({ id: "C", line1: "C - Lorem ipsum dolor", line2: "sit amet", counter: 0 });
+    this.nodes.push({ id: "D", line1: "D - Lorem ipsum dolor", line2: "sit amet", counter: 0 });
+    this.nodes.push({ id: "E", line1: "E - Lorem ipsum dolor", line2: "sit amet", counter: 0 });
+    this.nodes.push({ id: "F", line1: "F - Lorem ipsum dolor", line2: "sit amet", counter: 0 });
+    this.nodes.push({ id: "G", line1: "G - Lorem ipsum dolor", line2: "sit amet", counter: 0 });
+    this.nodes.push({ id: "H", line1: "H - Lorem ipsum dolor", line2: "sit amet", counter: 0 });
+
+    this.links.push({ sourceId: "A", destId: "B", counter: 0 });
+    this.links.push({ sourceId: "A", destId: "C", counter: 0 });
+    this.links.push({ sourceId: "B", destId: "D", counter: 0 });
+    this.links.push({ sourceId: "C", destId: "D", counter: 0 });
+    this.links.push({ sourceId: "D", destId: "E", counter: 0 });
+    this.links.push({ sourceId: "E", destId: "F", counter: 0 });
+    this.links.push({ sourceId: "F", destId: "G", counter: 0 });
+    this.links.push({ sourceId: "A", destId: "G", counter: 0 });
+    this.links.push({ sourceId: "G", destId: "H", counter: 0 });
+
+    this.mermaidGraphDefinition = this.sanitizer.bypassSecurityTrustHtml(this.getMermaidGraphDefinition());
   }
 
   ngOnInit(): void {
@@ -131,6 +171,7 @@ export class MermaidTestsComponent implements OnInit, AfterViewInit {
 
   onNodeMouseOver(event: MouseEvent) {
     const target = event.currentTarget as HTMLElement;
+    console.log("target node id " + target.id);
     target.querySelectorAll('rect, circle').forEach((element) => {
       const el = element as SVGElement;
       el.style.transform = 'scale(1.2)';
@@ -160,4 +201,33 @@ export class MermaidTestsComponent implements OnInit, AfterViewInit {
       svgElement.style.maxWidth = `${width}px`;
     }
   }
+
+  getMermaidGraphDefinition(): string {
+    const graphLines: string[] = [];
+    this.links.forEach(link => {
+      const sourceNode: MermaidNode = this.nodes.find(x => x.id == link.sourceId) ??
+        { id: link.sourceId, line1: "", line2: "", counter: 0 };
+      const destNode: MermaidNode = this.nodes.find(x => x.id == link.destId) ??
+        { id: link.destId, line1: "", line2: "", counter: 0 };
+      const sourceText1 = `<span class="mnodel1">${sourceNode.line1}</span>`;
+      const sourceText2 = `<span class="mnodel1">${sourceNode.line2}</span>`;
+      const destText1 = `<span class="mnodel2">${destNode.line1}</span>`;
+      const destText2 = `<span class="mnodel2">${destNode.line2}</span>`;
+      const line: string = `${sourceNode.id}([${sourceText1}<br/>${sourceText2}])-->${destNode.id}([${destText1}<br/>${destText2}])`
+
+      console.log(line);
+
+      graphLines.push(line);
+    });
+    const result: string = `<div id="mermaid" class="mermaid flex-grow-1 justify-content-center">graph TD; ${graphLines.join('; ')}</div>`;
+    console.log(result);
+
+    return result;
+  }
+
+  // mermaidGraphDefinition: string = `<div id="mermaid" class="mermaid flex-grow-1 justify-content-center">graph TD; </div>`;
 }
+
+// ids
+// line: id="L_A_B_0"
+// node: id="flowchart-A-0"
