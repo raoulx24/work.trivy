02d124be61b9f8255039b687dc3002aaf48ff1fa
raoulx24 <raoulx24@yahoo.com>
2024-11-27 23:49:16 +0200

    nighty night

--- src/TrivyOperator.Dashboard/ClientApp/src/app/fcose/fcose.component.ts
+++ src/TrivyOperator.Dashboard/ClientApp/src/app/fcose/fcose.component.ts
@@ -1,10 +1,13 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
-import cytoscape from 'cytoscape';
+import cytoscape, { ElementDefinition } from 'cytoscape';
 import fcose from 'cytoscape-fcose';
 cytoscape.use(fcose);
 
-
+// tests.sbom
+import { ClusterSbomReportService } from '../../api/services/cluster-sbom-report.service';
+import { ClusterSbomReportDto } from '../../api/models/cluster-sbom-report-dto';
+//
 
 @Component({
   selector: 'app-fcose',
@@ -13,36 +16,77 @@ cytoscape.use(fcose);
   templateUrl: './fcose.component.html',
   styleUrl: './fcose.component.scss'
 })
-export class FcoseComponent implements AfterViewInit {
+export class FcoseComponent {
   @ViewChild('graphContainer', { static: true }) graphContainer: ElementRef;
 
-  constructor() { }
+  constructor(private service: ClusterSbomReportService) {
+    // tests.sbom
+    this.getTableDataDtos();
+    //
+  }
 
-  ngAfterViewInit() {
+
+  // tests.sbom
+  getTableDataDtos() {
+    this.service.getClusterSbomReportDtos().subscribe({
+      next: (res) => this.onGetDataDtos(res),
+      error: (err) => console.error(err),
+    });
+  }
+
+  onGetDataDtos(dtos: ClusterSbomReportDto[]) {
+    this.dataDtos = dtos;
+
+    const elements: ElementDefinition[] = [];
+
+    this.dataDtos[0].details?.forEach(detail => {
+      elements.push({ data: { id: detail.bomRef, label: detail.name } });
+      detail.dependsOn?.forEach(depends => {
+        elements.push({
+          data: {
+            source: detail.bomRef,
+            target: depends
+          }
+        });
+      });
+    });
+
     const cy = cytoscape({
       container: this.graphContainer.nativeElement,
-      elements: [
-        { data: { id: 'a' } },
-        { data: { id: 'b' } },
-        { data: { id: 'c' } },
-        { data: { id: 'd' } },
-        { data: { id: 'e' } },
-        { data: { source: 'a', target: 'b' } },
-        { data: { source: 'a', target: 'c' } },
-        { data: { source: 'c', target: 'd' } },
-        { data: { source: 'd', target: 'e' } }],
+      elements: elements,
       layout: {
         name: 'fcose',
       },
       style: [
         {
           selector: 'node',
-          style: { 'background-color': '#666', 'label': 'data(id)' }
+          style: { 'background-color': '#44A', 'label': 'data(label)' }
         },
         {
           selector: 'edge',
           style: { 'width': 3, 'line-color': '#ccc', 'target-arrow-color': '#ccc', 'target-arrow-shape': 'triangle' }
         }]
     });
+
+    //cy.on('position', 'node', (event) => {
+    //  console.log("mama");
+
+    //  const sourceNode = event.target;
+    //  console.log(sourceNode);
+
+    //  const dependentNodes = sourceNode.connectedNodes();
+
+    //  dependentNodes.forEach((node: any) => {
+    //    console.log("tata");
+    //    node.position({
+    //      x: sourceNode.position().x + 10,
+    //      y: sourceNode.position().y + 10 
+    //    });
+    //  });
+    //});
+
   }
+
+  private dataDtos: ClusterSbomReportDto[] = [];
+  //
 }
