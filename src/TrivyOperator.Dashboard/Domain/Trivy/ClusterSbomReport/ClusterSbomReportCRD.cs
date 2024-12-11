d7f69589579a083d8712a8056998d460e4d726a0
raoulx24 <raoulx24@yahoo.com>
2024-12-12 00:36:42 +0200

    merge master in feature fix

--- src/TrivyOperator.Dashboard/Domain/Trivy/ClusterSbomReport/ClusterSbomReportCRD.cs
+++ src/TrivyOperator.Dashboard/Domain/Trivy/ClusterSbomReport/ClusterSbomReportCRD.cs
@@ -6,7 +6,7 @@ public class ClusterSbomReportCrd : CustomResourceDefinition
 {
     public override string Version { get; } = "v1alpha1";
     public override string Group { get; } = "aquasecurity.github.io";
-    public override string PluralName { get; } = "clustersbomreports";
+    public override string PluralName { get; } = "sbomreports";
     public override string Kind { get; } = "CResource";
     public override string? Namespace { get; init; } = null;
 }
